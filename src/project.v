714ff3c9bf5d7799d7aed77b7f53838cec916c52
Steve Jenson <stevej@gmail.com>
2025-02-28 20:42:25 -0800

    assign unused bits

--- src/project.v
+++ src/project.v
@@ -21,6 +21,8 @@ module tt_um_i2c_fnv1a_hasher (
   // All output pins must be assigned. If not used, assign to 0.
   //assign uo_out = ui_in + uio_in;  // Example: ou_out is the sum of ui_in and uio_in
   wire sda;
+  logic [4:0] unused_uio_in;
+  assign unused_uio_in = {uio_in[7:4], uio_in[0]};
   assign uio_out = {1'b0, sda, 6'b00_0000};
   assign uo_out = {8'b0000_0000};
 
@@ -41,6 +43,6 @@ module tt_um_i2c_fnv1a_hasher (
   );
 
   // List all unused inputs to prevent warnings
-  wire _unused = &{ena, 1'b0};
+  wire _unused = &{ena, ui_in, 1'b0};
 
 endmodule
